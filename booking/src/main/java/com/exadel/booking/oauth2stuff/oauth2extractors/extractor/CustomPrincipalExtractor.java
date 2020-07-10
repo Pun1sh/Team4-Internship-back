cc8769688a47ecd47794238523ebec7ffbd9d768
pun1sher_ <thepunisherofnoobs@gmail.com>
2020-07-10 19:07:58 +0300

    no message

---
+++ booking/src/main/java/com/exadel/booking/oauth2stuff/oauth2extractors/extractor/CustomPrincipalExtractor.java
@@ -0,0 +1,13 @@
+package com.exadel.booking.oauth2stuff.oauth2extractors.extractor;
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
+
+import java.util.Map;
+
+public class CustomPrincipalExtractor implements PrincipalExtractor {
+
+    @Override
+    public Object extractPrincipal(Map<String, Object> map) {
+        return map.get("name");
+    }
+}
