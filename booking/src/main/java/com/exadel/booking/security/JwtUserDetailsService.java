ac32a48fca0c1be6a19aa7c44ab0625e648a17ec
pun1sher_ <thepunisherofnoobs@gmail.com>
2020-07-11 13:58:54 +0300

    no message

---
+++ booking/src/main/java/com/exadel/booking/security/JwtUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.exadel.booking.security;
+
+import com.exadel.booking.model.User;
+import com.exadel.booking.security.jwt.JwtUser;
+import com.exadel.booking.security.jwt.JwtUserFactory;
+import com.exadel.booking.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class JwtUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Autowired
+    public JwtUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Autowired
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user  = userService.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User with username: " + username + " not found");
+        }
+
+        JwtUser jwtUser = JwtUserFactory.create(user);
+        log.info("IN loadUserByUsername - user with username: {} successfully loaded", username);
+        return jwtUser;
+
+    }
+}
