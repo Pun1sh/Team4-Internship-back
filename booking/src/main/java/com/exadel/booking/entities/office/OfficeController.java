f897d075061e2c750170728e6dc6de1ecad37814
pun1sher_ <thepunisherofnoobs@gmail.com>
2020-07-26 13:22:04 +0300

    security

--- booking/src/main/java/com/exadel/booking/entities/office/OfficeController.java
+++ booking/src/main/java/com/exadel/booking/entities/office/OfficeController.java
@@ -3,6 +3,7 @@ package com.exadel.booking.entities.office;
 import com.exadel.booking.entities.office.floor.FloorDto;
 import com.exadel.booking.entities.office.floor.FloorService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,16 +21,19 @@ public class OfficeController {
     private final OfficeService officeService;
     private final FloorService floorService;
 
+    @PreAuthorize("hasAuthority('OFFICE_READ')")
     @GetMapping
     public Collection<OfficeDto> getAllOffices() {
         return officeService.getAllOffices();
     }
 
+    @PreAuthorize("hasAuthority('OFFICE_READ')")
     @GetMapping(value = "/{id}")
     public OfficeDto getOfficeById(@PathVariable UUID id) {
         return officeService.getOfficeById(id);
     }
 
+    @PreAuthorize("hasAuthority('FLOOR_READ')")
     @GetMapping(value = "/{id}/floors")
     public List<FloorDto> getAllFloorsByOfficeId(@PathVariable UUID id) {
         return floorService.getAllFloorsByOfficeId(id);
--- booking/src/main/java/com/exadel/booking/entities/office/floor/room/RoomController.java
+++ booking/src/main/java/com/exadel/booking/entities/office/floor/room/RoomController.java
@@ -3,6 +3,7 @@ package com.exadel.booking.entities.office.floor.room;
 import com.exadel.booking.entities.office.floor.room.place.PlaceDto;
 import com.exadel.booking.entities.office.floor.room.place.PlaceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +19,13 @@ public class RoomController {
     private final RoomService roomService;
     private final PlaceService placeService;
 
+    @PreAuthorize("hasAuthority('ROOM_READ')")
     @GetMapping(value = "/{id}")
     public RoomDto getRoomById(@PathVariable UUID id) {
         return roomService.getRoomById(id);
     }
 
+    @PreAuthorize("hasAuthority('PLACE_READ')")
     @GetMapping(value = "/{id}/places")
     public List<PlaceDto> getAllPlacesByRoomId(@PathVariable UUID id) {
         return placeService.getAllPlacesByRoomId(id);
