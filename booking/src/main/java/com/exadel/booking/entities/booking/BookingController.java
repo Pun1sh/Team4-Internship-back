f774403b1b214ec49b32d9ae3ebe3e39ceb88467
Nastiatv <57249641+Nastiatv@users.noreply.github.com>
2020-08-12 13:30:49 +0300

    +

--- booking/src/main/java/com/exadel/booking/entities/booking/BookingController.java
+++ booking/src/main/java/com/exadel/booking/entities/booking/BookingController.java
@@ -21,13 +21,13 @@ public class BookingController {
 
     @PreAuthorize("hasAuthority('BOOKING_READ')")
     @GetMapping
-    public List<BookingDto> getAllBookingsByUserId(@PathVariable UUID userId) {
+    public List<BookingDto> getAllBookingsByUserId(UUID userId) {
         return bookingService.getAllBookingsByUserId(userId);
     }
 
     @PreAuthorize("hasAuthority('BOOKING_READ')")
-    @GetMapping(params = "/userId")
-    public Page<BookingDto> getAllActiveBookingsByUserId(@PathVariable UUID userId, LocalDateTime now, @PageableDefault(sort = {"lastName"}) Pageable pageable) {
+    @GetMapping(params = "userId", path = "/active")
+    public Page<BookingDto> getAllActiveBookingsByUserId(@RequestParam("userId")  UUID userId, LocalDateTime now, @PageableDefault(sort = {"lastName"}) Pageable pageable) {
         return bookingService.getAllActiveBookingsByUserId(userId, now, pageable);
     }
 
@@ -39,31 +39,31 @@ public class BookingController {
 
     @PreAuthorize("hasAuthority('BOOKING_WRITE')")
     @PostMapping
-    public BookingDto createBooking(@PathVariable UUID placeId,@PathVariable UUID userId, LocalDateTime bookingDate, LocalDateTime dueDate) {
+    public BookingDto createBooking(UUID placeId, UUID userId, LocalDateTime bookingDate, LocalDateTime dueDate) {
         return bookingService.createBooking(placeId, userId, bookingDate, dueDate);
     }
 
     @PreAuthorize("hasAuthority('BOOKING_WRITE')")
     @PutMapping
-    public BookingDto updateBooking(@PathVariable UUID bookingId, LocalDateTime bookingDate, LocalDateTime dueDate) {
+    public BookingDto updateBooking(UUID bookingId, LocalDateTime bookingDate, LocalDateTime dueDate) {
         return bookingService.updateBookingTime(bookingId, bookingDate, dueDate);
     }
 
     @PreAuthorize("hasAuthority('BOOKING_DELETE')")
     @DeleteMapping
-    public void deleteBooking(@PathVariable UUID bookindId) {
+    public void deleteBooking(UUID bookindId) {
         bookingService.deleteBookingById(bookindId);
     }
 
     @PreAuthorize("hasAuthority('BOOKING_READ')")
-    @GetMapping(params = "placeId")
-    public Boolean IsFree(@PathVariable UUID placeId, LocalDateTime bookingDate, LocalDateTime dueDate) {
+    @GetMapping(params = "placeId", path = "byplace")
+    public Boolean IsFree( @RequestParam("placeId") UUID placeId, LocalDateTime bookingDate, LocalDateTime dueDate) {
         return bookingService.checkDateTimeIsFreeWithoutUser(placeId, bookingDate, dueDate);
     }
 
     @PreAuthorize("hasAuthority('BOOKING_READ')")
-    @GetMapping(params = "roomId")
-    public List<BookingDto> getAllBookingsByRoomIdOnDate(@PathVariable UUID roomId, LocalDateTime start, LocalDateTime end) {
+    @GetMapping(params = "roomId", path = "byroom")
+    public List<BookingDto> getAllBookingsByRoomIdOnDate( @RequestParam("roomId")  UUID roomId, LocalDateTime start, LocalDateTime end) {
         return bookingService.getAllBookingsByRoomId(roomId, start, end);
     }
 }
