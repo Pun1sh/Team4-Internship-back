99e61e081d4062a4bafa07ab81aa700d2d96063a
pun1sher_ <thepunisherofnoobs@gmail.com>
2020-08-16 22:41:36 +0300

    filter heroku

---
+++ booking/src/main/java/com/exadel/booking/security/jwt/AccessControlAllowOriginFilter.java
@@ -0,0 +1,22 @@
+package com.exadel.booking.security.jwt;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class AccessControlAllowOriginFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        res.addHeader("Access-Control-Allow-Origin", "*");
+        res.addHeader("Access-Control-Allow-Headers", "Content-Type");
+        res.addHeader("Access-Control-Allow-Headers", "Authorization");
+        chain.doFilter(req, res);
+    }
+}
