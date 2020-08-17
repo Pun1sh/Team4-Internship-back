922ab0d0353db7fd6e30735d8098543e8d21774b
pun1sher_ <thepunisherofnoobs@gmail.com>
2020-08-17 14:24:43 +0300

    filter change

--- booking/src/main/java/com/exadel/booking/security/config/SecurityConfig.java
+++ booking/src/main/java/com/exadel/booking/security/config/SecurityConfig.java
@@ -6,10 +6,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
@@ -61,10 +59,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .deleteCookies("JSESSIONID")
                 .clearAuthentication(true);
     }
-
-    @Override
-    public void configure(final WebSecurity web) {
-        web.ignoring().antMatchers(HttpMethod.OPTIONS);
-    }
-
 }
